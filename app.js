9dd09ac2a017d1d59a752a5d99ee7cf8ffb5dca9
Eason <leoling127@gmail.com>
2019-05-20 13:49:10 +0800

    add main index page router-index

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-
 mongoose.connect('mongodb://localhost/shorten_url', { useNewUrlParser: true })
 const db = mongoose.connection
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: false }))
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -14,9 +19,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.get('/', (req, res) => {
-  res.send('hi')
-})
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
   console.log(`App is running in http://localhost:${port}`)
